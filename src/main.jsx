df602607244cc1707c98b2bc1f0f17134305fe25
RitvikSh <praveensiarma@gmail.com>
2023-09-07 21:50:01 +0530

    inital setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Animate from '../Animate.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Animate />
     <App />
   </React.StrictMode>,
 )
