17fe8a545d3d6fe01bf13d941666d5a99fe064bb
RitvikSh <praveensiarma@gmail.com>
2023-09-08 22:13:02 +0530

    removed tailwind and moved to standard css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import '../dist/output.css';
 
 function App() {
 
 
   return (
     <>
-      <div>
-        <div>
-          <div className="text-xl font-medium text-white">ChitChat</div>
-          <p className="text-slate-500 bold text-white">You have a new message!</p>
-        </div>
+      <div className="bg-black">
+        <div className="text-xl bg-black font-medium text-white">ChitChat</div>
+        <p className="text-slate-500 bold text-white">You have a new message!</p>
       </div>
     </>
   )
