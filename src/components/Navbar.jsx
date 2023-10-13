d7cd11cd5ca92af3c2c4743858a9f7eae45adfef
RitvikSh <praveensiarma@gmail.com>
2023-10-13 23:21:36 +0530

    added navbar and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-
+import Logo from '../assets/Logo.png';
+import { Link } from 'react-scroll';
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div>
+            <nav className="navbar">
+                <img className="logo" src={Logo} alt="logo" />
+                <div className="deskmenu">
+                    <Link className="deskmenulist">Home</Link>
+                    <Link className="deskmenulist">About</Link>
+                    <Link className="deskmenulist">Projects</Link>
+                    <Link className="deskmenulist">Skills</Link>
+                </div>
+                <button className="deskmenubt">
+                    <img src="" alt="" className="deskmenuing" />
+                    Contact Me
+                </button>
+            </nav>
+        </div>
     )
 }
 
