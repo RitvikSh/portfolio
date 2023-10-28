efecf17ad686a08a271be8e4c3287069c32487fb
RitvikSh <praveensiarma@gmail.com>
2023-10-28 13:54:05 +0530

    basic layout of about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,19 @@
 import "./about.css";
+import portfolio from "../../assets/portfolio.png"
 
 function About() {
     return (
-        <div>
-            About me
+        <div className="aboutsection">
+            <div className="aboutmetext">
+                <span className="aboutme1">
+                    Hi, <span className="aboutName">Ritvik</span> here
+                </span>
+                <p className="aboutme2">
+                    I am software engineer.
+                </p>
+                <button className="resume">resume</button>
+            </div>
+            <img src={portfolio} className="aboutimg" alt="that's how I look" />
         </div>
     )
 }
