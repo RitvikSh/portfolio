b7822c11c610d8d3afea71cba83865ff24dc4873
RitvikSh <praveensiarma@gmail.com>
2023-10-30 22:46:49 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/navbar/Navbar.jsx';
 import About from './components/about/About.jsx';
-import Projects from './components/Projects.jsx';
-import Skills from './components/Skills.jsx';
+import Projects from './components/projects/Projects.jsx';
+import Skills from './components/Experience.jsx';
 import Contact from './components/Contact.jsx';
 
 
